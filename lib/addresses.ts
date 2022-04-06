5b954fc54eb2c28b3c022b525e6686dd67b17334
kherin.eth <kherincalderon@gmail.com>
2022-04-06 01:49:18 -0600

    Solana Pay Tutotial

---
+++ lib/addresses.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js"
+
+// this is your shop wallet address
+export const shopAddress = new PublicKey('6htvwWGWTPvjahwzedfZEL3u9ZyAD3d86JFcAhaafLBU')
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('87sUJKvWWDfBq9uk2LGaro4c2DugAVDAx5iY5dGNof92')
---
+++ lib/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 'sour-diesel',
+    name: 'Sour Diesel',
+    description: 'Sour Diesel, also known as "Sour D", is a popular sativa marijuana strain. Sour Diesel effects are dreamy, cerebral, fast-acting and energizing.',
+    unitName: 'gram', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.05,
+    priceUsd: 5,
+  },
+  {
+    id: 'pre-roll-stash',
+    name: 'King Kush Hemp Flower',
+    description: '7 CBD hemp spliffs sealed for safety and to ensure the freshest, smoothest smoke. Hemp Flower contains less than .3% Total THC. CBD content 17.8%',
+    unitName: 'pack',
+    priceSol: 0.1,
+    priceUsd: 10,
+  }
+]
